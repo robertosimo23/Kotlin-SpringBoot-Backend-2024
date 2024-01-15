f2539b918bfd4065b8ae7679fae8043387efd261
Roberto Simões <simoes9523@outlook.com>
2024-01-15 18:54:49 -0400

    Third  Commit

---
+++ credit-application-system/credit-application-system/src/main/kotlin/dev/Roberto/Simoes/credit/applicationsystem/CreditApplicationSystemApplication.kt
@@ -0,0 +1,11 @@
+package dev.Roberto.Simoes.credit.applicationsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditApplicationSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditApplicationSystemApplication>(*args)
+}
---
+++ credit-application-system/credit-application-system/src/test/kotlin/dev/Roberto/Simoes/credit/applicationsystem/CreditApplicationSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.Roberto.Simoes.credit.applicationsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditApplicationSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
